a82bf78604a7896196d96d027b72e448fac31bc7
j-milkovits <j.milkovits.t@posteo.net>
2023-06-30 23:07:16 +0200

    add neoformat to nvim

---
+++ nvim/.config/nvim/after/plugin/neoformat.lua
@@ -0,0 +1,2 @@
+# neoformat should use project-local version of prettier
+vim.g.neoformat_try_node_exe = 1
--- nvim/.config/nvim/lua/main/packer.lua
+++ nvim/.config/nvim/lua/main/packer.lua
@@ -32,6 +32,7 @@ return require('packer').startup(function(use)
             {'hrsh7th/nvim-cmp'},     -- Required
             {'hrsh7th/cmp-nvim-lsp'}, -- Required
             {'L3MON4D3/LuaSnip'},     -- Required
-    }
-}   
+    }}
+    use ('sbdchd/neoformat')
+       
 end)
