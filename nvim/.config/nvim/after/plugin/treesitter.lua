be6b3bafb9aef9bbf13924619feb7a3745587b98
j-milkovits <j.milkovits.t@posteo.net>
2023-07-01 16:47:36 +0200

    remove autotag

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,9 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     sync_install = false,
     auto_install = true,
-    autotag = {
-        enable = true,
-    },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
